d7ea444dc6c02ee4994ad3388a3758428adff29d
kyle01 <kylemcv@gmail.com>
2022-03-25 01:49:08 -0400

    Init npm

--- javascript.js
+++ javascript.js
@@ -1,1 +1,6 @@
 console.log('hello world')
+
+const contents = document.getElementById('contents')
+const li = document.createElement("li");
+li.appendChild(document.createTextNode("Third post"));
+contents.appendChild(li);

f9e0c6a6b2272ff72a011c9a001b939579d545fa
kyle01 <kylemcv@gmail.com>
2022-03-26 18:56:17 -0400

    Generate pages for all writings in directory

--- generate_script.js
+++ generate_script.js
@@ -4,36 +4,41 @@ const showdown = require('showdown');
 
 const convertor = new showdown.Converter();
 const contentList = [];
-const rl = readline.createInterface({
-    input: fs.createReadStream('./writings/test_post.md'),
+
+fs.readdirSync('./writings/').forEach((file) => {
+  console.log(file)
+  const rl = readline.createInterface({
+    input: fs.createReadStream(`./writings/${file}`),
     crlfDelay: Infinity
   });
 
-let lineNumber = 0;
-let path = ""
-
-rl.on('line', (line) => {
-    lineNumber++;
-    if (lineNumber === 1) {
-      const title = line.toString().slice(2);
-      path = line.toString().slice(2).replace(" ", "_").trim().toLowerCase();
-      contentList.push({
-        "text": title,
-        "path": path
-      });
-      if (!fs.existsSync(`./posts/${path}`)){
-        fs.mkdirSync(`./posts/${path}`);
+  let lineNumber = 0;
+  let path = ""
+  rl.on('line', (line) => {
+      lineNumber++;
+      if (lineNumber === 1) {
+        const title = line.toString().slice(2);
+        path = line.toString().slice(2).replace(" ", "_").trim().toLowerCase();
+        contentList.push({
+          "text": title,
+          "path": path
+        });
+        if (!fs.existsSync(`./posts/${path}`)){
+          fs.mkdirSync(`./posts/${path}`);
+        }
+        const contentsJson = {
+          "contents": contentList
+        }
+        fs.writeFileSync('./contents.json', '')
+        fs.appendFileSync('./contents.json', JSON.stringify(contentsJson));
       }
-      const contentsJson = {
-        "contents": contentList
-      }
-      fs.writeFileSync('./contents.json', '')
-      fs.appendFileSync('./contents.json', JSON.stringify(contentsJson));
-    } 
-});
+  });
 
-rl.on('line', (line) => {
-    const html = convertor.makeHtml(line);
+  rl.on('line', (line) => {
+      const html = convertor.makeHtml(line);
 
-    fs.appendFileSync(`./posts/${path}/index.html`, html);
-});
+      fs.appendFileSync(`./posts/${path}/index.html`, html);
+  });
+})
+
+

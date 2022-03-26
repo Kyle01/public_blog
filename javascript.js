7de40305bd98d518345a86f110800e3245afa041
kyle01 <kylemcv@gmail.com>
2022-03-26 12:06:09 -0400

    Attempt dynamic contents

--- javascript.js
+++ javascript.js
@@ -2,3 +2,12 @@ const contents = document.getElementById('contents')
 const li = document.createElement("li");
 li.appendChild(document.createTextNode("Third post"));
 contents.appendChild(li);
+
+fetch('./contents.json').then(response => response.json()).then(json => {
+    console.log(json);
+    json.forEach(element => {
+        const li = document.createElement("li");
+        li.appendChild(document.createTextNode(element));
+        contents.appendChild(li);
+    });
+});
